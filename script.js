e70ea15ae8c1177dd30084ae729810fd74ac69a9
reterrr <sharonovnikol1@gmail.com>
2024-10-03 09:26:43 +0200

    initial commit 1_1

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-(function () {
-  //TODO
-})();
+function ex1_button() {
+  // Tworzenie ciągu liczb od 0 do 9 oddzielonych przecinkami
+  let numbers = Array.from({ length: 10 }, (_, i) => i).join(", ");
+  
+  // Umieszczanie ciągu liczb w elemencie o ID ex1_content
+  document.getElementById('ex1_content').textContent = numbers;
+}
+
