00b7baf9e3a9bd99f2527aa3c5054c7236e1ec56
Marria Rybak <marjarybak@gmail.com>
2024-10-03 09:41:13 +0200

    Ex1-Add an event listener to the button with ID ex1_button

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 (function () {
-  //TODO
+  const ex1Button = document.getElementById('ex1_button');
+  const ex1Content = document.getElementById('ex1_content');
+  
+  ex1Button.addEventListener('click', function () {
+    const numbers = Array.from({ length: 10 }, (_, i) => i).join(', ');
+    ex1Content.textContent = numbers;
+  });
 })();
